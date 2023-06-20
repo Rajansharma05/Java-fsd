cc8c975da5aab57f9f06fc41910dba03d8b3d46b
Rajansharma05 <rajan.sharma_cs18@gla.ac.in>
2023-06-20 14:19:40 +0530

    Here i use simple operators to perform some arithmetic operation like add, subtract , multiply , division.

---
+++ src/Practice_project/ArthmeticCalculator.java
@@ -0,0 +1,48 @@
+package Practice_project;
+
+import java.util.Scanner;
+
+public class ArthmeticCalculator {
+
+    public static void main(String[] args) {
+        // Create a Scanner object for user input
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the first number: ");
+        double num1 = scanner.nextDouble();
+
+        System.out.print("Enter the second number: ");
+        double num2 = scanner.nextDouble();
+
+        System.out.println("Select an operation: ");
+        System.out.println("1. Addition (+)");
+        System.out.println("2. Subtraction (-)");
+        System.out.println("3. Multiplication (*)");
+        System.out.println("4. Division (/)");
+
+        System.out.print("Enter your choice (1-4): ");
+        int choice = scanner.nextInt();
+
+        double result = 0.0;
+
+        switch (choice) {
+            case 1:
+                result = num1 + num2;
+                break;
+            case 2:
+                result = num1 - num2;
+                break;
+            case 3:
+                result = num1 * num2;
+                break;
+            case 4:
+                result = num1 / num2;
+                break;
+            default:
+                System.out.println("Invalid choice!");
+                return;
+        }
+
+        System.out.println("Result: " + result);
+    }
+}
