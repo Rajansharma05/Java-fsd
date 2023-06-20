d75f879e8e18b15a47897d7da18ea3b5787323e1
Rajansharma05 <rajan.sharma_cs18@gla.ac.in>
2023-06-20 09:55:54 +0530

    Modifiers

--- src/AssistedPractice/Modifier.java
+++ src/AssistedPractice/Modifier.java
@@ -4,7 +4,44 @@ public class Modifier {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		MyClass obj = new MyClass();
+		
+		obj.publicMethod();
+		System.out.println(obj.publicVariable);
+		
+		obj.protectedMethod();
+		System.out.println(obj.protectedVariable);
 
+		obj.defaultMethod();
+		System.out.println(obj.defaultVariable);
+
+
+	}
+
+}
+
+class MyClass{
+	public String publicVariable = "I am Public";
+	protected String protectedVariable = "I am protected";
+	String defaultVariable = "I am default";
+	private String privateVariable = "I am private";
+	
+	public void publicMethod() {
+		System.out.println("This is public method");
+	}
+	
+	public void protectedMethod() {
+		System.out.println("This is public method");
+		
+	}
+    void defaultMethod() {
+		System.out.println("This is public method");
+		
+	}
+    private void privateMethod() {
+		System.out.println("This is public method");
+		
 	}
 
 }
